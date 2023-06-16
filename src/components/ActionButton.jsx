14e00560defb0d744e574d6e91da49837013c51d
cesardmn <cesardmn@gmail.com>
2023-06-16 09:50:29 -0300

    crete action button

---
+++ src/components/ActionButton.jsx
@@ -0,0 +1,5 @@
+import styles from '@styles/ActionButton.module.css'
+
+export default function ActionButton({ children }) {
+  return <button className={styles.actionButton}> {children}</button>
+}
